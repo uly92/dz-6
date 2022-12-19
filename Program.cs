66d5e69f747b63e51c45f380cbdfbfe0e22d5b25
Uuljan <uly-9292@mail.ru>
2022-12-19 16:55:00 +0700

    dz6

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿//  Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь
+ 
+ /*
+Console.Write("Input a number separated by ",": ");
+int[] numbers = StringToNum(Console.ReadLine());
+PrintArray(numbers);
+int sum = 0;
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] > 0)
+    {
+        sum++;
+    }
+}
+Console.WriteLine();
+Console.WriteLine($"result 0 = {sum}");
+*/
+
+// Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+
+Console.WriteLine("input b1 ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("input a number k1: ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("input  b2 ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("input a number k2:");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double x = (-b2 + b1)/(-k1 + k2);
+double y = k2 * x + b2;
+
+Console.WriteLine($"result: X: {x}, Y: {y}");
+
